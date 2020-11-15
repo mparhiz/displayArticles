54c49cb60dfdbb5962e9c3e25ddc4b0c44349c1d
Mohsen Parhiz <m_parhiz@yahoo.com>
2020-11-15 23:46:11 +1100

    applied the article modal and fixed card size

--- src/mockData/articlesData.js
+++ src/mockData/articlesData.js
@@ -3,8 +3,8 @@ const articlesData = [
 		title: 'Love of learning',
 		date: "14 Nov, 2020",
 		body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-		description: `Ut enim ad minim veniam, quis nostrud exercitation
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			Ut enim ad minim veniam, quis nostrud exercitation
 			ullamco laboris nisi ut aliquip ex ea commodo consequat.
 			Duis aute irure dolor in reprehenderit in voluptate velit
 			esse cillum dolore eu fugiat nulla pariatur. Excepteur
@@ -16,8 +16,8 @@ const articlesData = [
 		date: "14 Nov, 2020",
 		body: `Lorem ipsum dolor sit amet, usu eu zril adipiscing. Ea mea quod
 			bonorum persecuti. An enim voluptua usu. Vis mentitum scribentur
-			necessitatibus ad, sea te assum lucilius, tale ullum ad qui.`,
-		description: `Vim alia ocurreret honestatis eu, ne eripuit atomorum
+			necessitatibus ad, sea te assum lucilius, tale ullum ad qui.
+			Vim alia ocurreret honestatis eu, ne eripuit atomorum
 			conclusionemque ius, mei ex luptatum sadipscing. Pri te nonumy
 			oportere, at simul pericula cum. Eu euismod delicata per. Te assum
 			nihil vocent sed, duo voluptua phaedrum te. Ut mea cetero repudiandae
@@ -28,8 +28,8 @@ const articlesData = [
 		date: "13 Nov, 2020",
 		body: `Ad dicit civibus euripidis quo. An sit iisque sadipscing.
 			Qui aeque deserunt eu, ut ignota latine malorum per, tale graeco
-			atomorum cum in.`,
-		description: `Ne iriure perpetua periculis has, ut inani phaedrum
+			atomorum cum in.
+			Ne iriure perpetua periculis has, ut inani phaedrum
 			ullamcorper vim, feugiat senserit eam id. Ne tempor voluptatibus
 			sed, te sanctus adolescens has, ad duo sumo elitr prompta.`
 	},
@@ -37,8 +37,8 @@ const articlesData = [
 		title: 'Love of learning',
 		date: "13 Nov, 2020",
 		body: `Eum mundi quidam fabellas ea. Vim in prompta maiorum, vim
-			percipit menandri appellantur ex, an sint idque ornatus his.`,
-		description: `Per ei mazim putant minimum. Ne soluta fabulas vis,
+			percipit menandri appellantur ex, an sint idque ornatus his.
+			Per ei mazim putant minimum. Ne soluta fabulas vis,
 			nec vivendo suscipit et, mei paulo altera ne. Has te ornatus
 			rationibus inciderint, ius ei sonet dolore integre, an ignota
 			vidisse nam.`
@@ -50,17 +50,15 @@ const articlesData = [
 			Ad quas homero deserunt sit, quas noster comprehensam usu ei.
 			Nonumes liberavisse vim ex. Aliquip probatus delicatissimi in nec,
 			ex splendide deterruisset nec. Purto velit scribentur ius te, ut
-			dolore nostrum qui.Eum mundi quidam fabellas ea.`,
-		description: `Vim in prompta maiorum, vim percipit menandri
-			appellantur ex, an sint idque ornatus his.`
+			dolore nostrum qui.Eum mundi quidam fabellas ea. Vim in prompta
+			maiorum, vim percipit menandri appellantur ex, an sint idque ornatus his.`
 	},
 	{
 		title: 'Love of learning',
 		date: "12 Nov, 2020",
 		body: `Ad dicit civibus euripidis quo. An sit iisque sadipscing.
 			Qui aeque deserunt eu, ut ignota latine malorum per, tale graeco
-			atomorum cum in.`,
-		description: `Ne iriure perpetua periculis has, ut inani phaedrum
+			atomorum cum in. Ne iriure perpetua periculis has, ut inani phaedrum
 			ullamcorper vim, feugiat senserit eam id. Ne tempor voluptatibus
 			sed, te sanctus adolescens has, ad duo sumo elitr prompta.`
 	},
@@ -71,17 +69,15 @@ const articlesData = [
 			Ad quas homero deserunt sit, quas noster comprehensam usu ei.
 			Nonumes liberavisse vim ex. Aliquip probatus delicatissimi in nec,
 			ex splendide deterruisset nec. Purto velit scribentur ius te, ut
-			dolore nostrum qui.Eum mundi quidam fabellas ea.`,
-		description: `Vim in prompta maiorum, vim percipit menandri
-			appellantur ex, an sint idque ornatus his.`
+			dolore nostrum qui.Eum mundi quidam fabellas ea. Vim in prompta
+			maiorum, vim percipit menandri appellantur ex, an sint idque ornatus his.`
 	},
 	{
 		title: 'Curious mind leads the way for carer Jill',
 		date: "24 Oct, 2020",
 		body: `Ad dicit civibus euripidis quo. An sit iisque sadipscing.
 			Qui aeque deserunt eu, ut ignota latine malorum per, tale graeco
-			atomorum cum in.`,
-		description: `Ne iriure perpetua periculis has, ut inani phaedrum
+			atomorum cum in. Ne iriure perpetua periculis has, ut inani phaedrum
 			ullamcorper vim, feugiat senserit eam id. Ne tempor voluptatibus
 			sed, te sanctus adolescens has, ad duo sumo elitr prompta.`
 	}
